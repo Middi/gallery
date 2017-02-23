bbd98f0f4f97632324212fae7de470864d1fa2ee
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-23 14:50:24 +0100

    created modals for all cards

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,18 @@
 // MODAL
+
+var modal = document.querySelector(".modal");
+var modalBg = document.querySelector(".modal-bg");
+var closeButton = document.querySelector(".close-button");
+var openImg = document.getElementsByClassName("open-img");
+
+closeButton.addEventListener("click", function() {
+  modal.classList.toggle("closed");
+  modalBg.classList.toggle("closed");
+});
+
+for (var i = 0; i < openImg.length; i++) {
+	openImg[i].addEventListener('click', function(){
+		modal.classList.toggle("closed");
+    modalBg.classList.toggle("closed");
+	});
+}

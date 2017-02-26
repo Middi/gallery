90fbfa1effb6324aa585223c0534bd0e5b72f96e
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-26 16:58:44 +0100

    images load from target

--- assets/main.js
+++ assets/main.js
@@ -4,20 +4,29 @@
 var modal = document.querySelector(".modal");
 var modalBg = document.querySelector(".modal-bg");
 var closeButton = document.querySelector(".close-button");
-
-
+var closeBg = document.querySelector(".modal-bg");
 var openImg = document.getElementsByClassName("open-img");
 
-
-
+// Event listeners
 closeButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
   modalBg.classList.toggle("closed");
 });
 
-// Iterate through and add event listeners
+closeBg.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalBg.classList.toggle("closed");
+});
+
+// Iterate through and add event listeners to open-img
 for (var i = 0; i < openImg.length; i++) {
-  openImg[i].addEventListener('click', function () {
+  openImg[i].addEventListener('click', function (e) {
+
+    console.log(e.target);
+    
+    document.getElementById('img-modal').src = e.target.src;
+
+
     modal.classList.toggle("closed");
     modalBg.classList.toggle("closed");
   });

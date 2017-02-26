6f293bb3865d66032325cc975f9e6f83a36eab7f
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-26 15:45:08 +0100

    psuedo for pulling images

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,29 @@
 // MODAL
 
+// Grab from DOM
 var modal = document.querySelector(".modal");
 var modalBg = document.querySelector(".modal-bg");
 var closeButton = document.querySelector(".close-button");
+
+
 var openImg = document.getElementsByClassName("open-img");
 
-closeButton.addEventListener("click", function() {
+
+
+closeButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
   modalBg.classList.toggle("closed");
 });
 
+// Iterate through and add event listeners
 for (var i = 0; i < openImg.length; i++) {
-	openImg[i].addEventListener('click', function(){
-		modal.classList.toggle("closed");
+  openImg[i].addEventListener('click', function () {
+    modal.classList.toggle("closed");
     modalBg.classList.toggle("closed");
-	});
+  });
 }
+
+// Get image from card
+
+// set it to Modal image
+
